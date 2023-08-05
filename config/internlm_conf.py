f7b29c989ff70b435d4e63c6ea80389c347a9168
ssbuild <462304@qq.com>
2023-08-05 08:41:21 +0800

    add config
    Signed-off-by: ssbuild <462304@qq.com>

--- config/internlm_conf.py
+++ config/internlm_conf.py
@@ -25,6 +25,27 @@ internlm_conf = {
                 # "your_adapter_name": "/data/nlp/pre_models/torch/your_adapter_dir",
             }
         }
+    },
+    "internlm-chat-7b-int4": {
+        "enable": False,
+        "work_mode": "deepspeed",  # one of deepspeed,accelerate,hf
+        "workers": [
+            {
+                "device_id": [0]  # 默认启动一个worker , 使用第一块显卡
+            }
+        ],
 
+        "auto_quantize": True,  # 是否自动量化模型
+        "model_config": {
+            "model_type": "internlm",
+            "model_name_or_path": "/data/nlp/pre_models/torch/internlm/internlm-chat-7b-int4",
+            "use_fast_tokenizer": False,
+            "do_lower_case": None,
+            "lora": {
+                # 多个 lora , adapter_name: lora weight dir
+                # "default": "/data/nlp/pre_models/torch/lora_path_dir",
+                # "your_adapter_name": "/data/nlp/pre_models/torch/your_adapter_dir",
+            }
+        }
     },
 }
--- config/qwen_conf.py
+++ config/qwen_conf.py
@@ -52,4 +52,28 @@ qwen_conf = {
 
     },
 
+    "qwen-7b-chat-int4": {
+        "enable": False,
+        "work_mode": "deepspeed",  # one of deepspeed,accelerate,hf
+        "workers": [
+            {
+                "device_id": [0]  # 默认启动一个worker , 使用第一块显卡
+            }
+        ],
+
+        "auto_quantize": True, # 是否自动量化模型
+        "model_config": {
+            "model_type": "qwen",
+            "model_name_or_path": "/data/nlp/pre_models/torch/qwen/qwen-7b-chat-int4",
+            "use_fast_tokenizer": False,
+            "do_lower_case": None,
+            "lora": {
+                # 多个 lora , adapter_name: lora weight dir
+                # "default": "/data/nlp/pre_models/torch/lora_path_dir",
+                # "your_adapter_name": "/data/nlp/pre_models/torch/your_adapter_dir",
+            }
+        }
+
+    },
+
 }
