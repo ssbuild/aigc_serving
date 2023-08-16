208f96da6f653e0af9ffb4364bf3ced0ac4f4b2f
ssbuild <462304@qq.com>
2023-08-16 20:43:34 +0800

    update
    Signed-off-by: ssbuild <462304@qq.com>

--- config/main.py
+++ config/main.py
@@ -24,10 +24,10 @@ __all__ = [
 # 资源充足可以全部启用 , 并导入 global_models_info_args
 global_models_info_args = {
     # **baichuan_config,
-    # **baichuan2_config
+    # **baichuan2_config,
     # **bloom_conf,
     # **chatglm_conf,
-    # **chatglm2_conf
+    # **chatglm2_conf,
     # **internlm_conf,
     # **llama_conf,
     # **moss_conf,
