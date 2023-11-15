4c6dba5c29e213a718951422c21a305a9226dca5
ssbuild <462304@qq.com>
2023-11-15 22:39:10 +0800

    padantic>=2
    Signed-off-by: ssbuild <462304@qq.com>

--- serving/serve/api_keys.py
+++ serving/serve/api_keys.py
@@ -1,27 +1,23 @@
 # -*- coding: utf-8 -*-
 # @Author  : ssbuild
 # @Time    : 2023/8/11 14:29
-import typing
-import pydantic
+from typing import List, Optional
 from fastapi import Depends, HTTPException
 from fastapi.security import HTTPAuthorizationCredentials, HTTPBearer
-#兼容旧版本
-if pydantic.__version__.split('.')[0] == '1':
-    from pydantic import BaseSettings
-else:
-    from pydantic.v1 import BaseSettings
+from pydantic.v1 import BaseSettings
+    
 
 class AppSettings(BaseSettings):
     # The address of the model controller.
-    api_keys: typing.List[str] = None
+    api_keys: List[str] = None
 
 app_settings = AppSettings()
 headers = {"User-Agent": "aigc_serving"}
 get_bearer_token = HTTPBearer(auto_error=False)
 
 def auth_api_key(
-    auth: typing.Optional[HTTPAuthorizationCredentials] = Depends(get_bearer_token),
-) -> typing.Optional[str]:
+    auth: Optional[HTTPAuthorizationCredentials] = Depends(get_bearer_token),
+) -> Optional[str]:
     if app_settings.api_keys:
         if auth is None or (token := auth.credentials) not in app_settings.api_keys:
             raise HTTPException(
